ae10b04c25c839d9c4def45631de42e69331de35
Davi Figueiredo <davifigu21@gmail.com>
2023-12-06 22:15:58 -0300

    classe (Matches) - Models
    implementado script de models e forms à classe matches.

--- apps/matches/models.py
+++ apps/matches/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from games.models import Game
 
 # Create your models here.
+
+class Match(models.Model):
+    jogo = models.CharField('Jogo', max_length=50)
+    dia = models.CharField('Dia', max_length=50)
+    hora = models.CharField('Hora', max_length=50)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = 'Partida'
+        verbose_name_plural = 'Partidas'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
