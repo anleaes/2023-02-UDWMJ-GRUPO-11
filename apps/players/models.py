e260efa0769d1a1410142ffc7c48f71c2c670f22
Davi Figueiredo <davifigu21@gmail.com>
2023-12-06 22:43:13 -0300

    classe (Playerinventories) - Models
    Implementada classe dentro de classe "players"
    
    implementado script models à classe playerinventories.

--- apps/players/models.py
+++ apps/players/models.py
@@ -9,10 +9,7 @@ class Player(models.Model):
     last_name = models.CharField('Sobrenome', max_length=100) 
     address = models.CharField('Endereco', max_length=200)   
     email = models.EmailField('E-mail',null=False, blank=False)
-
-# NÃO ESQUECER DE ADICIONAR "inventory" acima
-
-# ESPAÇO PARA CLASSE INVENTORY
+    player_inventory = models.ManyToManyField(Item, through='PlayerInventory', blank=True)
 
     class Meta:
         verbose_name = 'Jogador'
@@ -22,4 +19,14 @@ class Player(models.Model):
     def __str__(self):
         return self.first_name
 
-# ESPAÇO PARA CLASSE INVENTORY
+class PlayerInventory(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Inventario'
+        verbose_name_plural = 'Inventarios'
+        ordering =['id']
+
+    def __str__(self):
+        return self.item.name
