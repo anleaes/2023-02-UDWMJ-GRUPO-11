bc9d8318d7c7b92142c20541b3ef0bbd2cebb882
Davi Figueiredo <davifigu21@gmail.com>
2023-12-06 22:05:53 -0300

    classe (Playeratches)
    Implementado script "url" à classe Playermatches.

--- projetogamesapp/settings.py
+++ projetogamesapp/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'matches.apps.MatchesConfig',
+    'playermatches.apps.PlayerMatchesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- projetogamesapp/urls.py
+++ projetogamesapp/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('jogadores em partidas/', include('playermatches.urls', namespace='playermatches')),
     path('partidas/', include('matches.urls', namespace='matches')),
     path('jogos/', include('games.urls', namespace='games')),
     path('itens/', include('items.urls', namespace='items')),
