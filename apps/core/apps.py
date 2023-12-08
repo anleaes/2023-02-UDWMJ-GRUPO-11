e904ab91373487cc273e1df715cc51c13c69cc06
Guilherme Herberto de Pieri Lemos <guilhermeherberto3@gmail.com>
2023-12-07 23:33:39 -0300

    2.1
    Ajuste de verbose_name.

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-    verbose_name = 'Nome do seu App'
+    verbose_name = 'Game Creator'
